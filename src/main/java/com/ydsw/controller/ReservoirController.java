6f4035741ef2d6bef4777138a407d0b18220bed8
Jayny-Plus <Jayny-Plus@15515203518@163.com>
2025-05-13 16:48:56 +0800

    文件上传补全

--- src/main/java/com/ydsw/controller/ReservoirController.java
+++ src/main/java/com/ydsw/controller/ReservoirController.java
@@ -4,12 +4,17 @@ import cn.hutool.json.JSONObject;
 import com.fengwenyi.api.result.ResultTemplate;
 import com.ydsw.domain.Reservoir;
 import com.ydsw.service.ReservoirService;
+import com.ydsw.utils.ShpfileUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -24,5 +29,24 @@ public class ReservoirController {
         Reservoir reservoir=jsonObject.toBean(Reservoir.class);
         List<Map<String,Object>> res=reservoirService.selectReservoirByConditons(reservoir);
         return LakesController.getRangeFromPGgeometryforMap(res);
+    }
+
+    @PostMapping(value = "/api/reservoir/uploadByShpfiles")
+    public ResultTemplate<Object> uploadByShpfiles(@RequestParam("shpfiles") MultipartFile[] fileGroup) {
+        if (fileGroup == null || fileGroup.length == 0) {
+            return ResultTemplate.fail("请提交文件！");
+        }
+        try {
+        List<Reservoir> reservoirList = ShpfileUtils.parseMultipleShpGroups(fileGroup, Reservoir.class);
+        for (Reservoir reservoir : reservoirList) {
+            reservoir.getGeom().setSRID(4326);
+            reservoir.setCreateTime(new Date());
+            reservoir.setStatus(0);
+        }
+    } catch (IOException e) {
+        return ResultTemplate.fail("文件："+fileGroup[0].getOriginalFilename()+"提交格式错误！");
+    }
+
+        return ResultTemplate.success("提交成功！");
     }
 }
--- src/main/java/com/ydsw/controller/SluiceController.java
+++ src/main/java/com/ydsw/controller/SluiceController.java
@@ -3,8 +3,10 @@ package com.ydsw.controller;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import com.fengwenyi.api.result.ResultTemplate;
+import com.ydsw.domain.Reservoir;
 import com.ydsw.domain.Sluice;
 import com.ydsw.service.SluiceService;
+import com.ydsw.utils.ShpfileUtils;
 import lombok.extern.slf4j.Slf4j;
 import net.postgis.jdbc.PGgeography;
 import net.postgis.jdbc.PGgeometry;
@@ -12,8 +14,12 @@ import net.postgis.jdbc.geometry.Geometry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -40,5 +46,24 @@ public class SluiceController {
             map.put("geog",wkt);
         }
         return ResultTemplate.success(sluiceList);
+    }
+
+    @PostMapping(value = "/api/sluice/uploadByShpfiles")
+    public ResultTemplate<Object> uploadByShpfiles(@RequestParam("shpfiles") MultipartFile[] fileGroup) {
+        if (fileGroup == null || fileGroup.length == 0) {
+            return ResultTemplate.fail("请提交文件！");
+        }
+        try {
+            List<Sluice> sluiceList = ShpfileUtils.parseMultipleShpGroups(fileGroup, Sluice.class);
+            for (Sluice  sluice: sluiceList) {
+                sluice.getGeog().setSRID(4326);
+                sluice.setCreateTime(new Date());
+                sluice.setStatus(0);
+            }
+        } catch (IOException e) {
+            return ResultTemplate.fail("文件："+fileGroup[0].getOriginalFilename()+"提交格式错误！");
+        }
+
+        return ResultTemplate.success("提交成功！");
     }
 }
